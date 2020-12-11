b9af1a969717560fd6478c7fd36f95515971e907
Eider Arango <eider.a@hotmail.com>
2020-12-10 19:24:51 -0500

    changed endpoint

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -19,8 +19,8 @@ export default {
         this.username = this.$route.params.username
         
         let self = this
-        axios.get("http://127.0.0.1:60060/user/balance/" + this.username)
-        // axios.get("https://eider-arango-cajero-ui.herokuapp.com/user/balance/" + this.username)
+        // axios.get("http://127.0.0.1:60060/user/balance/" + this.username)
+        axios.get("https://eider-arango-cajero-ui.herokuapp.com/user/balance/" + this.username)
             .then((result) => {
                 // self.balance = result.data.balance
                 console.log(result.data);
